ea0708263e1635e182c8049fda4135bde3ad606f
Axel Espinosa <axeldavid45@gmail.com>
2025-07-29 20:59:21 -0600

    basic list

--- basic-lists.py
+++ basic-lists.py
@@ -36,3 +36,31 @@ print(letters)
 # only removes first ocurrence
 letters.remove('c')
 print(letters)
+
+# Sorting list
+cars = ['bmw', 'audi', 'subaru', 'toyota']
+cars.sort()
+print(cars)
+print('List sorted forever:', cars)
+cars = ['bmw', 'audi', 'subaru', 'toyota']
+
+cars.sort(reverse=True)
+print('Reversed order', cars)
+
+# Sort without modifying original
+cars = ['bmw', 'audi', 'subaru', 'toyota']
+print('cars original', cars)
+print('cars sorted',sorted(cars))
+print('cars sorted reverse',sorted(cars, reverse=True))
+print('cars original', cars)
+
+cars = ['bmw', 'audi', 'subaru', 'toyota']
+
+# reverse list in place
+print('cars original', cars)
+cars.reverse()
+print('cars reversed', cars)
+print('cars original', cars)
+
+# lenght
+print(len(cars))
