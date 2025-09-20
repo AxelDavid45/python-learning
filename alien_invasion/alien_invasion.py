8a6f8a84c3f1203e54d576edd440a10df2d59ccf
Axel Espinosa <axeldavid45@gmail.com>
2025-09-19 18:12:00 -0600

    feat: show ship in middbottom screen

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,36 @@
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game and create game resources"""
+        pygame.init()
+        # Clock for managing FPS
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien invasion")
+        self.ship = Ship(self)
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
